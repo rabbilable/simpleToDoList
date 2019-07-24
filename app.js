65a8ba2bc62e9bc3d151f34d79e3eff80c482f32
rabbilable <rabbilyasar@gmail.com>
2019-07-24 20:34:04 +0600

    finish delete

--- app.js
+++ app.js
@@ -1,6 +1,38 @@
 const form = document.querySelector('.add');
+const list = document.querySelector('.todos');
+
+const generateTemplate = todo => {
+    
+    const html = `
+        <li class="list-group-item d-flex justify-content-between align-items-center">
+        <span>${todo}</span>
+        <i class="far fa-trash-alt delete"></i>
+        </li>
+    `;
+
+    list.innerHTML += html;
+};
 
 form.addEventListener('submit', e => {
     e.preventDefault();
-    console.log(form.add.value)
-})
+    const todo = form.add.value.trim();
+
+    if(todo.length){
+        generateTemplate(todo);
+        form.reset();
+    }
+
+});
+
+// console.log();
+// console.log()
+
+//delete todos
+
+list.addEventListener('click', e => {
+    if(e.target.classlist.contains('delete')){
+        e.target.parentElement.remove();
+    }
+
+    // console.log(e.target.classlist);
+});
