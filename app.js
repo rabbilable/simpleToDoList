e676d1e5a3f4419a748445d4e050a7b1e9a4cf73
rabbilable <rabbilyasar@gmail.com>
2019-07-25 13:30:54 +0600

    add search

--- app.js
+++ app.js
@@ -38,17 +38,17 @@ list.addEventListener('click', e => {
 // search
 const filterTodos = (term) => {
     Array.from(list.children)
-    .filter(todo => !todo.textContent.includes(term))
+    .filter(todo => !todo.textContent.toLowerCase().includes(term))
     .forEach(todo => todo.classList.add('filtered'));
 
     Array.from(list.children)
-    .filter(todo => todo.textContent.includes(term))
+    .filter(todo => todo.textContent.toLowerCase().includes(term))
     .forEach(todo => todo.classList.remove('filtered'));
 };
 
 // keyup event
 
 search.addEventListener('keyup', () => {
-    const term = search.value.trim();
+    const term = search.value.trim().toLowerCase();
     filterTodos(term);
 });
